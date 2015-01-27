90f138b86501a92f325e39373d2ad36155d6f858
Kristina Hjertberg <hjert.k@gmail.com>
2015-01-26 16:51:35 -0800

    adding a hash for stationary locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Noise.destroy_all
 Perishable.destroy_all
 
 ## Add Stationary Locations
-def stationary_locations(file, noise_type, decibel, seasonal)
+def stationary_locations(noise_type, file, decibel, seasonal)
   results = HTTParty.get("https://data.seattle.gov/resource/#{file}.json").parsed_response
   results.each do |r|
     Noise.create(
@@ -20,14 +20,15 @@ def stationary_locations(file, noise_type, decibel, seasonal)
   puts "\n#{noise_type} Imported"  
 end
 
-# Fire Stations
-stationary_locations("znfv-apni", "Fire Station", 0, false)
+# Stationary Locations Hash
+stationary = {
+  "Fire Station" => { file: "znfv-apni", decibel: 0, seasonal: false },
+  "School" => { file: "pmap-kbvr", decibel: 0, seasonal: true },
+  "College" => { file: "qawk-qmwr", decibel: 0, seasonal: true },
+  "Trolley" => { file: "4qvq-uf9z", decibel: 70, seasonal: false }
+}
 
-# Public Schools
-stationary_locations("pmap-kbvr", "School", 0, true)
-
-# Higher Education
-stationary_locations("qawk-qmwr", "College", 0, true)
-
-# SLU Trolley Stops
-stationary_locations("4qvq-uf9z", "Trolley", 70, false)
+# Loop Through Locations
+stationary.each do |k, v|
+  stationary_locations(k, v[:file], v[:decibel], v[:seasonal])
+end
