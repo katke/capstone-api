37982d6819c96b74cd9ff1cc8cf27167d6b19545
Kristina Hjertberg <hjert.k@gmail.com>
2015-02-03 10:34:59 -0800

    adding basic Noise.nearby_noises method and one passing test

--- app/models/noise.rb
+++ app/models/noise.rb
@@ -3,4 +3,8 @@ class Noise < ActiveRecord::Base
   def self.get_score(latitude, longitude)
     "A"
   end
+
+  def self.nearby_noises(latitude, longitude)
+    Noise.where(lat: 47.902, lon: -122.9)
+  end
 end
--- spec/models/noise_spec.rb
+++ spec/models/noise_spec.rb
@@ -9,4 +9,15 @@ describe Noise do
       expect(result).to match(/[A-F]/)
     end
   end
+
+  describe "#nearby_noises" do
+
+    let!(:noise1) { Noise.create(lat: 47.902,lon: -122.9) }
+    let!(:noise2) { Noise.create(lat: 48,lon: -122.9) }
+
+    it "returns nearby locations" do
+      result = Noise.nearby_noises(47.9, -122.9)
+      expect(result).to eq([noise1])
+    end
+  end
 end
