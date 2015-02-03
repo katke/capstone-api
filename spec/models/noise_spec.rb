a18d4e7f8900f274947dc193641557ab3c5ca915
Kat <kmmpatke@gmail.com>
2015-02-03 11:06:58 -0800

    wrote test to confirm that noises .004 degrees and higher aren't captured

--- spec/models/noise_spec.rb
+++ spec/models/noise_spec.rb
@@ -13,12 +13,18 @@ describe Noise do
   describe "#nearby_noises" do
 
     let!(:in_range_noises) { [Noise.create(lat: 47.902, lon: -122.9), Noise.create(lat: 47.9, lon: -122.902)] }
-    let!(:out_of_range_noise) { Noise.create(lat: 48, lon: -122.9) }
+    let!(:out_of_range_noise) { Noise.create(lat: 47.904, lon: -122.904) }
 
 
     it "returns nearby locations" do
       result = Noise.nearby_noises(47.9, -122.9)
       expect(result).to eq(in_range_noises)
     end
+
+    it "does not include location that is .004 degrees away" do
+      result = Noise.nearby_noises(47.9, -122.9)
+      expect(result).not_to eq(:out_of_range_noise)
+    end
+
   end
 end
