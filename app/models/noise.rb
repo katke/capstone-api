32c039325a6691cf71c75da324a4d15a3c605f21
Kat <kmmpatke@gmail.com>
2015-02-03 14:27:57 -0800

    removed crazy long SQL query, and .004 lat/lon boundaries

--- app/models/noise.rb
+++ app/models/noise.rb
@@ -21,11 +21,6 @@ class Noise < ActiveRecord::Base
   end
 
   def self.nearby_noises(latitude, longitude)
-    # lat_upper_limit = latitude + 0.004
-    # lat_lower_limit = latitude - 0.004
-    # lon_upper_limit = longitude + 0.004
-    # lon_lower_limit = longitude - 0.004
-    # Noise.where("lat BETWEEN ? AND ? AND lon BETWEEN ? AND ?", lat_lower_limit, lat_upper_limit, lon_lower_limit, lon_upper_limit)
     Noise.near([latitude, longitude], 0.13)
   end
 
