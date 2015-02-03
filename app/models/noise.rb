defd412ead0e3a96a76526a00fcd80c71d97a5d4
Kat <kmmpatke@gmail.com>
2015-02-03 11:04:07 -0800

    fixed Kristina's hot mess of a test, also, wrote super long SQL query

--- app/models/noise.rb
+++ app/models/noise.rb
@@ -1,10 +1,14 @@
 class Noise < ActiveRecord::Base
-  
+
   def self.get_score(latitude, longitude)
     "A"
   end
 
   def self.nearby_noises(latitude, longitude)
-    Noise.where("lat = ? AND lon = ?", 47.902, -122.9)
+    lat_upper_limit = latitude + 0.004
+    lat_lower_limit = latitude - 0.004
+    lon_upper_limit = longitude + 0.004
+    lon_lower_limit = longitude - 0.004
+    Noise.where("lat BETWEEN ? AND ? AND lon BETWEEN ? AND ?", lat_lower_limit, lat_upper_limit, lon_lower_limit, lon_upper_limit)
   end
 end
