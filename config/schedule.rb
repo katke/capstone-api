84fb87d60c771a1077adcddbb5bf56f6fdd08a93
Kat <kmmpatke@gmail.com>
2015-02-17 10:25:36 -0800

    changed cron to update every 30 min

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 set :output, "/../lib/cron/cron_log.log"
 
-every 1.day, :at => '3:30 am' do
+# every 1.day, :at => '3:30 am' do
+#   rake 'update_data'
+# end
+
+every 30.minutes do
   rake 'update_data'
 end
