6813d79ed4e2804a2d5878674ad20f7d1c0773e7
Kristina Hjertberg <hjert.k@gmail.com>
2015-02-20 14:43:24 -0800

    updating test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe HomeController, :type => :controller do
 
   describe "GET #index" do
-    it "is successful" do
+    it "is redirected" do
       get :index
-      expect(response.status).to eq 200
+      expect(response.status).to eq 302
     end
   end
 
